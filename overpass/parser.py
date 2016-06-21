df5bd92e5d9b64b0c51417723eb1a08e0244ff82
Davide Gessa <gessadavide@gmail.com>
2016-06-21 13:46:38 +0200

    bound splitting

--- overpass/parser.py
+++ overpass/parser.py
@@ -197,9 +197,7 @@ def get_points(coin = 'bitcoin', iso = 'XBT'):
 	
 	for xbb in boundings:
 		print 'Processing boundings:', xbb
-		resp = requests.get('http://overpass.osm.rambler.ru/cgi/interpreter?data=[out:json][timeout:600];(node["payment:bitcoin"=yes]'+xbb+';way["payment:bitcoin"=yes]'+xbb+';>;);out;')
-	
-		#(node["payment:%(coin)s"=yes];>;way["payment:%(coin)s"=yes]'+xbb+';>;relation["payment:%(coin)s"=yes]'+xbb+';>;node["currency:%(iso)s"=yes]'+xbb+';>;way["currency:%(iso)s"=yes]'+xbb+';>;relation["currency:%(iso)s"=yes]'+xbb+';);out;' % {"coin": coin, "iso": iso});
+		resp = requests.get('http://overpass.osm.rambler.ru/cgi/interpreter?data=[out:json][timeout:600];(node["payment:%(coin)s"=yes];>;way["payment:%(coin)s"=yes]'+xbb+';>;relation["payment:%(coin)s"=yes]'+xbb+';>;node["currency:%(iso)s"=yes]'+xbb+';>;way["currency:%(iso)s"=yes]'+xbb+';>;relation["currency:%(iso)s"=yes]'+xbb+';);out;' % {"coin": coin, "iso": iso});
 	
 		#print resp, resp.text
 		try:
